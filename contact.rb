4af05d0e9bd093a5b6a0d2b124629eaa696bb043
Ariane Deschenes Foucher <ariane.df@me.com>
2016-04-04 01:33:13 -0400

    Installed sqlite3, activerecord and mini_record. Required active_record and mini_record in contact.rb

--- contact.rb
+++ contact.rb
@@ -3,6 +3,11 @@
 #
 #   ruby test/contact_test.rb
 ## If your implementation of the Contact class is 'right', then you should see that all tests pass
+require "active_record"
+require "mini_record"
+
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "crm-web.sqlite3")
+
 class Contact
   attr_accessor :first_name, :last_name, :email, :note
   attr_reader :id
