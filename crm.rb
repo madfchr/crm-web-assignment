27198e69a95940c932fdb1c61e57b2fc5c5e217e
Ariane Deschenes Foucher <ariane.df@me.com>
2016-04-03 20:06:07 -0400

    Implemented View All Contacts

--- crm.rb
+++ crm.rb
@@ -2,9 +2,18 @@
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
 
+require_relative 'contact'
 require 'sinatra'
 
 get '/' do      # '/' matches the top level of my website
   @crm_app_name = "Ariane's CRM"
   erb :index
+end
+
+get '/contacts' do
+  Contact.create('Ariane', 'Foucher', 'ariane@me.com', 'Loves coding')
+  Contact.create('Kim', 'Oleiro', 'kim@her.com', 'New Bitmaker bestie')
+  Contact.create('Arthur', 'Chayka', 'arthur@universe.ca', 'New Tinder crush')
+
+erb :contacts
 end
